8fb9b5d227beba97f7c102bf1f48c575fd20a995
PJOGomes <36425309+PJOGomes@users.noreply.github.com>
2019-11-20 09:00:18 +0000

    Deleted System Class and created manager Class

---
+++ src/com/PauloGomes/Manager.java
@@ -0,0 +1,17 @@
+package com.PauloGomes;
+
+import java.util.ArrayList;
+
+public class Manager {
+    public static void print(ArrayList<?> list) {
+
+    }
+
+    public static void report (ArrayList<ActiveProgrammer> programmmers, ArrayList<ProjectTeam> teams) {
+
+    }
+
+    public static void updateDate (ArrayList<ActiveProgrammer> programmmers, ArrayList<ProjectTeam> teams) {
+
+    }
+}
