ce93ff90687a1ac5de615666d02c2d9b91234c3d
PJOGomes <1500480@estudante.uab.pt>
2019-11-25 06:54:34 +0000

    Menu comment added

--- src/com/PauloGomes/Menu.java
+++ src/com/PauloGomes/Menu.java
@@ -11,7 +11,7 @@ public class Menu {
 
     private static Date sysDate= new Date();
 
-
+    //Getter and Setter
     public static Date getSysDate() {
         return sysDate;
     }
@@ -20,12 +20,19 @@ public class Menu {
         this.sysDate = sysDate;
     }
 
+    //Function: loadMenu
+    //Description: function to display a progress bar to represent a mock load bar progress and to request the user's password to access the program
+    //
+    //@Input: An arrayList of type ActiveProgrammer and an arrayList of type ProjectTeam
+    //
+    //@Output: no output
     public static void loadMenu(ArrayList<ActiveProgrammer> programmers, ArrayList<ProjectTeam> teams) throws java.lang.Exception{
         Scanner scanner = new Scanner(System.in);
         String insertedPassword;
         int i = 0;
         System.out.println("Please wait while the program loads");
         System.out.println("-----------------------------------\n\n\n");
+        //Create loadbar
         while(i < 21) {
             System.out.print("    [");
             for (int j=0;j<i;j++) {
@@ -39,6 +46,7 @@ public class Menu {
             System.out.print("] "+  i*5 + "%");
             if(i<20) {
                 System.out.print("\r");
+                //Adjust the duration
                 Thread.sleep(250);
             }
             i++;
@@ -47,6 +55,7 @@ public class Menu {
         System.out.println("Database loaded.\nPlease enter password to access Program or quit to exit:");
         int count = 0;
         while(true){
+            //Get the password inserted by user
             insertedPassword = scanner.nextLine();
             if(insertedPassword.equals("quit")) {
                 System.out.println("Exiting Program");
@@ -56,10 +65,12 @@ public class Menu {
                 mainMenu(programmers, teams);
             } else {
                 count++;
+                //If password is wrong
                 if(count<3)
                 {
                     System.out.println("Wrong Password!\nYou have tried "+count+ " time(s). You only have " +(3-count)+" left.\nPlease enter password to access Program or quit to exit:");
                 } else {
+                    //If the user failed the password more than three times
                     System.out.println("You have exceded the number of tries. The program will now close");
                     System.exit(-2);
                 }
@@ -67,6 +78,12 @@ public class Menu {
         }
     }
 
+    //Function: mainMenu
+    //Description: function to load the database and to give the user options to access submenus
+    //
+    //@Input: An arrayList of type ActiveProgrammer and an arrayList of type ProjectTeam
+    //
+    //@Output: no output
     public static void mainMenu(ArrayList<ActiveProgrammer> programmers, ArrayList<ProjectTeam> teams) throws ParseException, IOException {
         Scanner scanner = new Scanner(System.in);
         String option;
@@ -75,12 +92,15 @@ public class Menu {
         Manager manager = new Manager();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         CRUDDatabase database = new CRUDDatabase();
+        //Load the info from the xml file used as database
         database.readFile(programmers, teams);
+        //If the database doesn´t have the minimum requirements load the mockup database
         if(programmers.size()<4||teams.size()<2){
             System.out.println("The database does not have the minimal requirements\nA mockup database will be uploaded");
             database.readMockup(programmers, teams);
         }
         Menu menu = new Menu();
+        //Get the system date saved in the database
         menu.setSysDate(CRUDDatabase.readDate());
         String today = dateFormat.format(getSysDate());
         boolean exit = false;
@@ -124,6 +144,7 @@ public class Menu {
                    companyMenu(programmers, teams);
                 case "0":
                     System.out.println("Thank you for using our Program");
+                    //Save the current info in teh xml Files
                     CRUDDatabase.saveDate();
                     CRUDDatabase.saveExit();
                     System.exit(0);
@@ -133,6 +154,12 @@ public class Menu {
         }
     }
 
+    //Function: programmerMenu
+    //Description: function to display to the user several options about programmer related options
+    //
+    //@Input: An arrayList of type ActiveProgrammer and an arrayList of type ProjectTeam
+    //
+    //@Output: no output
     private static void programmerMenu(ArrayList<ActiveProgrammer> programmers, ArrayList<ProjectTeam> teams) throws ParseException, IOException {
         Scanner scanner = new Scanner(System.in);
         String option;
@@ -175,6 +202,12 @@ public class Menu {
 
     }
 
+    //Function: projectMenu
+    //Description: function to display to the user several options about project related options
+    //
+    //@Input: An arrayList of type ActiveProgrammer and an arrayList of type ProjectTeam
+    //
+    //@Output: no output
     private static void projectMenu(ArrayList<ActiveProgrammer> programmers, ArrayList<ProjectTeam> teams) throws ParseException, IOException {
         Scanner scanner = new Scanner(System.in);
         String option;
@@ -216,6 +249,12 @@ public class Menu {
         }
     }
 
+    //Function: companyMenu
+    //Description: function to display to the user several options about company related options
+    //
+    //@Input: An arrayList of type ActiveProgrammer and an arrayList of type ProjectTeam
+    //
+    //@Output: no output
     private static void companyMenu(ArrayList<ActiveProgrammer> programmers, ArrayList<ProjectTeam> teams) throws ParseException, IOException {
         Scanner scanner = new Scanner(System.in);
         String option;
@@ -242,11 +281,13 @@ public class Menu {
                     break;
                 case "3":
                     System.out.println("The database from the last system exit will be reloaded");
+                    //Load backup will undo all changes made in the current session
                     CRUDDatabase.loadBackup();
                     CRUDDatabase.readFile(programmers, teams);
                     subMenu();
                     break;
                 case "4":
+                    //Get the projects in the history xml file and print it
                     ArrayList<ProjectTeam> historyList = new ArrayList<>();
                     CRUDDatabase.readHistory(historyList);
                     Manager.printHistory(historyList);
@@ -261,7 +302,12 @@ public class Menu {
         }
     }
 
-
+    //Function: subMenu
+    //Description: function to display to the user the option to retunr to the last menu used or exit the program
+    //
+    //@Input:
+    //
+    //@Output: no output
     private static void subMenu() throws IOException {
         Scanner scanner = new Scanner(System.in);
         String option;
