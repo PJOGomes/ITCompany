251d910240f179f2615ea77c9a123cfed4b4b4fa
PJOGomes <1500480@estudante.uab.pt>
2019-11-25 06:23:25 +0000

    Main comment added

--- src/com/PauloGomes/Main.java
+++ src/com/PauloGomes/Main.java
@@ -6,9 +6,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws java.lang.Exception{
+        //Create the arrayList that will contain the company's programmers
         ArrayList<ActiveProgrammer> programmers = new ArrayList<>();
+        //Create the arrayList that will contain the company's projects
         ArrayList<ProjectTeam> projects = new ArrayList<>();
         Menu menu = new Menu();
+        //Load the startup menu
         menu.loadMenu(programmers, projects);
     }
 
